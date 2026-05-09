962d9b3d01bd52278733433cfc10be2ffbb4e1b6
Evie <kirazrtwo@gmail.com>
2026-05-10 04:37:40 +0800

    fix: suppress image selection rectangle via CSS

--- components/ClickCat.tsx
+++ components/ClickCat.tsx
@@ -40,7 +40,7 @@ export function ClickCat() {
               width={90}
               height={90}
               draggable={false}
-              className="select-none object-contain"
+              className="click-cat-img object-contain"
               style={{
                 filter:
                   "brightness(0) invert(1) drop-shadow(0 0 6px #fff) drop-shadow(0 0 12px rgba(216,180,254,1)) drop-shadow(0 0 30px rgba(168,85,247,0.9)) drop-shadow(0 0 50px rgba(168,85,247,0.5))",
